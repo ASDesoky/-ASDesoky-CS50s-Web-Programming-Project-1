010133f029b27f03e505fd440162800a791298d5
Assiya Desoky <134889666+ASDesoky@users.noreply.github.com>
2024-10-02 19:31:43 -0400

    Create views.py

---
+++ views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from . import util
+
+def entry(request, title):
+    content = util.get_entry(title)
+    if content is None:
+        return render(request, "encyclopedia/error.html", {
+            "message": "The requested page was not found."
+        })
+    return render(request, "encyclopedia/entry.html", {
+        "title": title,
+        "content": content
+    })

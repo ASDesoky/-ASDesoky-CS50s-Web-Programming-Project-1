19cec4c53b562225874467722b9d47d660337e41
Assiya Desoky <134889666+ASDesoky@users.noreply.github.com>
2024-10-02 19:34:20 -0400

    Create randompageviews.py

---
+++ randompageviews.py
@@ -0,0 +1,7 @@
+# views.py
+import random
+
+def random_page(request):
+    entries = util.list_entries()
+    random_entry = random.choice(entries)
+    return redirect("entry", title=random_entry)

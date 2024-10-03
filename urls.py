8ebf8b9cd20e1184d7f6cd9539a28eed7033a397
Assiya Desoky <134889666+ASDesoky@users.noreply.github.com>
2024-10-03 12:34:42 -0400

    made changes.

--- urls.py
+++ urls.py
@@ -3,9 +3,9 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("wiki/<str:title>", views.entry, name="entry"),
-    path("search", views.search, name="search"),
-    path("create", views.create, name="create"),
-    path("edit/<str:title>", views.edit, name="edit"),
-    path("random", views.random_page, name="random_page"),
+    path("wiki/<str:entry>", views.entry, name="entry"),
+    path("newEntry", views.newEntry, name="entry"),
+    path("wiki/<str:entry>/edit", views.edit, name="edit"), 
+    path("random", views.random, name="random"),
+    path("search", views.search, name="search")
 ]

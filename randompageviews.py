6915ecc2797c524c7776cb5e28164ab094fcd670
Assiya Desoky <134889666+ASDesoky@users.noreply.github.com>
2024-10-03 15:22:44 -0400

    Delete randompageviews.py

--- randompageviews.py
+++
@@ -1,8 +0,0 @@
-# views.py
-import random
-
-def random_page(request):
-    entries = util.list_entries()
-    random_entry = random.choice(entries)
-    return redirect("entry", title=random_entry)
-

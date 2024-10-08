9c635e02ae858b91d274eb113890888aa2c4f741
Assiya Desoky <134889666+ASDesoky@users.noreply.github.com>
2024-10-08 16:20:43 -0400

    made changes.

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -3,5 +3,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("wiki/<str:entry>", views.entry, name="entry"),
+    path("search/", views.search, name="search"),
+    path("createNewPage/", views.createNewPage, name="createNewPage"),
+    path("edit/", views.edit, name="edit"),
+    path("save/", views.save, name="save"),
+    path("randomPage/", views.randomPage, name="randomPage"),
 ]

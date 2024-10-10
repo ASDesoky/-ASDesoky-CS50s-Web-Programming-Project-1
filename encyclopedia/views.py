b1713ddcd2d7cd05c90f5186229c7c473a4d7ed1
Assiya Desoky <134889666+ASDesoky@users.noreply.github.com>
2024-10-09 20:08:49 -0400

    made changes

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -29,9 +29,9 @@ def newEntry(request):
         if form.is_valid():
             title = form.cleaned_data["title"]
             content = form.cleaned_data["content"]
-            if (util.get_entry (title) is None or form.cleane data ["edit"] is True)  
+            if (util.get_entry (title) is None or form.cleane data ["edit"] is True)
             return render(request, "encyclopedia/entry.html", {
-                "entry": apnaMarkdown.convert(content), 
+                "entry": apnaMarkdown.convert(content),  # type: ignore
                 "entryTitle": entry
             })
         else:
